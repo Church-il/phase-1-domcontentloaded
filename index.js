3496aa487215ca16a606c78ae6743030ded6ae7c
Michael Akoko <akokomichael37@gmail.com>
2024-08-03 12:44:42 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+
+  // Target the paragraph element with id="text"
+  const textElement = document.getElementById("text");
+
+  // Replace the text content
+  textElement.textContent = "This is really cool!";
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
